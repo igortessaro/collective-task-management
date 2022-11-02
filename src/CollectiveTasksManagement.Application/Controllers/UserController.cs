02464b1f9b80ed418be887783377cffbd1b5a2b7
Igor Tessaro <igorstessaro57@gmail.com>
2022-11-02 18:16:43 -0300

    feat: initial commit

---
+++ src/CollectiveTasksManagement.Application/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollectiveTasksManagement.Application.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
